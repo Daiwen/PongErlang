f5fe929749d6fa955dc78a762acbd66f9990964c
Quentin Lambert <lambert.quentin@gmail.com>
2013-11-26 18:07:18 +0100

    starting to implement graphic representation
    modified:   pong.erl
    	implemented: pong_str
    	new: pong_str_aux
    	new: line_str

--- src/pong.erl
+++ src/pong.erl
@@ -10,29 +10,78 @@ start_client(Server_Address) ->
     init_input_listener(Pid).
     
 
+%% TODO
+contact_server(Server_Address)->
+    .
+
+
 client_loop(Server_Address, Key) ->
     receive
 	request -> Server_Adress ! {self(), Key},
 		   client_loop(Server_Adress, unknown);
-	{frame, {Ball_Pos,
-		 {self(), My_Pos},
-		 {_, Foe_Pos}}} -> draw_frame(Ball_Pos, My_Pos, Foe_Pos),
+	{frame, {{Field_Size},
+		 Ball_Pos,
+		 My_Pos,
+		 Foe_Pos}} -> draw_frame(Field_Size,
+					      Ball_Pos,
+					      My_Pos,
+					      Foe_Pos),
 				   client_loop(Server_Address, Key);
+
 	{input, '\ESC'} -> ok;
 	{input, Key} -> client_loop(Server_Address, Key)
     end.
 
 
-drawframe({XB,YB}, {X1,Y1}, {X2,Y2}) ->
+%% TODO
+draw_frame({XG,YG}, {XB,YB}, X1, X2) ->
+    Str_Frame = pong_str({XG,YG}, {XB,YB}, X1, X2),
+    io:format("%p", [Str_Frame]).
+
+pong_str({XG,YG}, {XB,YB}, X1, X2) ->
+    V_line = string:chars($_,XG,"~n"),
+    H_line = pong_str_aux({XG,YG}, {XB,YB}, X1, X2, YG, ""),
+    Temp = string:concat(V_line, H_line),
+    string:concat(Temp,V_line).
+
+pong_str_aux(_, _, _, _, 0, Acc_Str) ->
+    Acc_Str;
+pong_str_aux({XG,YG}, {XB, 1}, X1, X2, 1, Acc_Str) ->
+    Temp_Str = line_str(XG, XB, X2),
+    pong_str_aux({XG,YG}, {XB, 1}, X1, X2, 0, concat(Temp_Str,Acc_Str));
+pong_str_aux({XG,YG}, {XB, YG}, X1, X2, YG, Acc_Str) ->
+    Temp_Str = line_str(XG, XB, X1),
+    pong_str_aux({XG,YG}, {XB, YG}, X1, X2, YG-1, concat(Temp_Str,Acc_Str));
+pong_str_aux({XG,YG}, Ball, X1, X2, YG, Acc_Str) ->
+    Temp_Str = line_str(XG, none, X1),
+    pong_str_aux({XG,YG}, Ball, X1, X2, YG-1, concat(Temp_Str,Acc_Str));
+pong_str_aux({XG,YG}, Ball, X1, X2, 1, Acc_Str) ->
+    Temp_Str = line_str(XG, none, X2),
+    pong_str_aux({XG,YG}, Ball, X1, X2, 0, concat(Temp_Str,Acc_Str));
+pong_str_aux({XG,YG}, {XB,YC}, X1, X2, YC, Acc_Str) ->
+    Temp_Str = line_str(XG, XB, none),
+    pong_str_aux({XG,YG}, {XB,YC}, X1, X2, YC-1, concat(Temp_Str,Acc_Str));
+pong_str_aux({XG,YG}, Ball, X1, X2, YC, Acc_Str) ->
+    Temp_Str = line_str(XG, none, none),
+    pong_str_aux({XG,YG}, Ball, X1, X2, YC-1, concat(Temp_Str,Acc_Str));
+    
+%%TODO
+line_str(XG, XB, X) ->
     .
+    
+    
 
-
+%% TODO
+init_input_listener(Pid)->
+    .
 
 start_server() ->
     cfg = read_config(),
     register(serverpong, spawn(?MODULE, init_server, [cfg])).
-    
 
+%%TODO    
+init_server(cfg)->
+    .
 
 stop_server() ->
     serverpong ! stop.
